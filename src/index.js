69880238307fbaa9beef35e9d76f8364d24f372b
GeetaSakthivel <geetha28tech@gmail.com>
2024-08-06 19:01:08 +0800

    new files and folders are created

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import {initializeApp} from 'firebase/app';
-import {getAuth, onAuthStateChanged} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-const firebaseApp = initializeApp({
+// My web app's Firebase configuration
+const firebaseConfig = {
     apiKey: "AIzaSyDJWlMmEcm4Yhb2DRpH7MmLgm4k4AHLJr0",
     authDomain: "student-teacher-booking-202ed.firebaseapp.com",
     projectId: "student-teacher-booking-202ed",
     storageBucket: "student-teacher-booking-202ed.appspot.com",
     messagingSenderId: "1055937679029",
     appId: "1:1055937679029:web:08e479cb266867d6ddb8fc",
-    measurementId: "G-3HEHGEEFFW" 
-});
-const auth = getAuth();
-const db= getFirestore();
+    measurementId: "G-3HEHGEEFFW"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
